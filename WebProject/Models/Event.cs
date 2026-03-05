56be59a4861dd4de41cfb2b7c6c7e7958d301c10
Skladanoff <skladanov@yandex.ru>
2026-03-05 22:54:46 +0400

    Add model (Event)

---
+++ WebProject/Models/Event.cs
@@ -0,0 +1,8 @@
+public class Event
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartAt { get; set; }
+    public DateTime EndAt { get; set; }
+}
