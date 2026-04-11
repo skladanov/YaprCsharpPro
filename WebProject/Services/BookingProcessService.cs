9cbf6380fab3d3b3c7edfa9f42444b40442a9188
Skladanoff <skladanov@yandex.ru>
2026-04-11 15:25:03 +0400

    Add logger to BookingProcessService

--- WebProject/Services/BookingProcessService.cs
+++ WebProject/Services/BookingProcessService.cs
@@ -1,14 +1,18 @@
 public class BookingProcessService : BackgroundService
 {
     private readonly IBookingRepository _bookingRepository;
+    private readonly ILogger<BookingProcessService> _logger;
 
-    public BookingProcessService(IBookingRepository bookingRepository)
+    public BookingProcessService(IBookingRepository bookingRepository, ILogger<BookingProcessService> logger)
     {
         _bookingRepository = bookingRepository;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Booking process service started.");
+
         while (!stoppingToken.IsCancellationRequested)
         {
             try
@@ -18,20 +22,29 @@ public class BookingProcessService : BackgroundService
                 var pendingBookings = await _bookingRepository.GetPendingBookings(stoppingToken);
                 if (pendingBookings?.Any() == true)
                 {
+                    _logger.LogInformation($"Processing {pendingBookings.Count()} pending bookings.");
+
                     pendingBookings.ForEach(booking =>
                     {
                         booking.Status = Booking.BookingStatus.Confirmed;
                         booking.ProcessedAt = DateTime.Now;
                     });
+
+                    _logger.LogInformation($"Successfully processed {pendingBookings.Count()} bookings.");
+                }
+                else
+                {
+                    _logger.LogDebug("No pending bookings found. Checking again in 2 seconds.");
                 }
             }
             catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
             {
+                _logger.LogInformation("Booking process service is stopping due to cancellation request.");
                 break;
             }
             catch (Exception ex)
             {
-
+                _logger.LogError(ex, "An error occurred while processing bookings: {ErrorMessage}", ex.Message);
             }
         }
     }
