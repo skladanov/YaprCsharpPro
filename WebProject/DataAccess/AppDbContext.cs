bc7deb85dbca9618efdbbe1d5ba866fab3f3d345
Skladanoff <skladanov2@yandex.ru>
2026-05-11 20:24:33 +0300

    Add DbContext

---
+++ WebProject/DataAccess/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebProject.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) :  base(options){}
+    
+    public DbSet<Booking>? bookings => Set<Booking>();
+    private DbSet<Event>? events => Set<Event>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
