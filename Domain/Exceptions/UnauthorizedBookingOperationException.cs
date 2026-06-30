1ffbb5286141e7edb6668ee5959e1e24fc3e2d0a
Skladanoff <skladanov2@yandex.ru>
2026-06-30 09:16:39 +0300

    Add UnauthorizedBookingOperationException.cs

---
+++ Domain/Exceptions/UnauthorizedBookingOperationException.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions
+{
+    public class UnauthorizedBookingOperationException : BusinessException
+    {
+        public UnauthorizedBookingOperationException()
+            : base("Unauthorized operation") { }
+
+        public UnauthorizedBookingOperationException(Guid userId)
+            : base($"Unauthorized booking operation for user with ID {userId}") { }
+    }
+}
