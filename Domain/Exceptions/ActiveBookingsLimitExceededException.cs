cc7a14ea1db77a337b8ca6de2e14c625ab49975e
Skladanoff <skladanov2@yandex.ru>
2026-06-30 09:15:18 +0300

    Add ActiveBookingsLimitExceededException.cs

---
+++ Domain/Exceptions/ActiveBookingsLimitExceededException.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+
+namespace Domain.Exceptions
+{
+    public class ActiveBookingsLimitExceededException : BusinessException
+    {
+        public ActiveBookingsLimitExceededException()
+            : base("User has exceeded the limit of active bookings") { }
+
+        public ActiveBookingsLimitExceededException(Guid userId)
+            : base($"User {userId} has exceeded the limit of active bookings") { }
+
+        public ActiveBookingsLimitExceededException(Guid bookingId, Guid userId)
+            : base($"User {userId} has exceeded the limit of active bookings with ID {bookingId}") { }
+    }
+}
