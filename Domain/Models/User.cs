b0063aae6e79504d17b757fd5adb120ee0cd928a
Skladanoff <skladanov2@yandex.ru>
2026-06-30 09:17:16 +0300

    Add Domain User Entity

---
+++ Domain/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        private User() { } // EF Core нужен пустой конструктор
+        public Guid Id { get; private set; }
+        public string Login { get; private set; } = null!;
+        public byte[] PasswordHash { get; private set; } = null!;
+        public UserRole Role { get; private set; }
+    }
+
+    public enum UserRole
+    {
+        User,
+        Admin
+    }
+}
