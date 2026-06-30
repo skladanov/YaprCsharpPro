df2d1bc321b052891c81a560ed181b2ba69be6c4
Skladanoff <skladanov2@yandex.ru>
2026-06-30 09:15:41 +0300

    Add BookingAlreadyCancelledException.cs

---
+++ Domain/Exceptions/BookingAlreadyCancelledException.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions
+{
+    public class BookingAlreadyCancelledException : BusinessException
+    {
+        public BookingAlreadyCancelledException()
+            : base("Booking was cancelled") { }
+
+        public BookingAlreadyCancelledException(Guid bookingId)
+            : base($"Booking with ID {bookingId} was cancelled") { }
+    }
+}
