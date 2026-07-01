2d0aff450f9458e7a08554780f6286930c929440
skladanov <skladanov2@yandex.ru>
2026-07-01 17:24:37 +0400

    Add PasswordHasher

---
+++ Application/Services/IPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Application.Services
+{
+    public interface IPasswordHasher
+    {
+        byte[] Hash(string password);
+
+        bool Verify(string password, byte[] storedHash);
+    }
+}
---
+++ Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public byte[] Hash(string password)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(password);
+        return sha256.ComputeHash(bytes);
+    }
+
+    public bool Verify(string password, byte[] storedHash)
+    {
+        var candidateHash = Hash(password);
+        return CryptographicOperations.FixedTimeEquals(candidateHash, storedHash);
+    }
+}
