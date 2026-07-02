fd9afd437568f768118914ebc3c81c7c7a1dafd5
skladanov <skladanov2@yandex.ru>
2026-07-02 14:16:22 +0400

    Rename UnauthorizedException

---
+++ Domain/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions
+{
+    public class UnauthorizedException : BusinessException
+    {
+        public UnauthorizedException()
+            : base("Unauthorized operation") { }
+
+        public UnauthorizedException(Guid userId)
+            : base($"Unauthorized booking operation for user with ID {userId}") { }
+    }
+}
