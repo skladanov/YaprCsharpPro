681abd395284c371be217a58a8bb95ae1d3c6b8f
skladanov <skladanov2@yandex.ru>
2026-07-02 14:16:48 +0400

    Add DuplicateLoginException.cs

---
+++ Domain/Exceptions/DuplicateLoginException.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions
+{
+    public class DuplicateLoginException : BusinessException
+    {
+        public DuplicateLoginException()
+            : base("The user is already registered") { }
+
+        public DuplicateLoginException(String login)
+            : base($"The user with login {login} is already registered") { }
+    }
+}
