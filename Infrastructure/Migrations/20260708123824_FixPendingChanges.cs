50a8b029236bfaa42b6151b4fcd885bc33064d17
Skladanoff <skladanov2@yandex.ru>
2026-07-09 21:37:52 +0300

    Update migrations

---
+++ Infrastructure/Migrations/20260708123824_FixPendingChanges.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "user_id",
+                table: "bookings",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    login = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    password = table.Column<byte[]>(type: "bytea", nullable: false),
+                    role = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_user_id",
+                table: "bookings",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_login",
+                table: "users",
+                column: "login",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_bookings_users_user_id",
+                table: "bookings",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_bookings_users_user_id",
+                table: "bookings");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_bookings_user_id",
+                table: "bookings");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "bookings");
+        }
+    }
+}
