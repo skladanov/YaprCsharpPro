4f13533df7a367f8a303a80cc250e1bd31ee8c03
Skladanoff <skladanov2@yandex.ru>
2026-07-21 20:58:33 +0300

    BookingService. Fix KafkaOptions

--- BookingService/Infrastructure/Options/KafkaOptions.cs
+++ BookingService/Infrastructure/Options/KafkaOptions.cs
@@ -2,7 +2,6 @@
 {
     public string BootstrapServer { get; set; } = "string.Empty";
     public string ClientId { get; set; } = string.Empty;
-    public string TopicPrefix { get; set; } = string.Empty;
     public string GroupId { get; set; } = "bookings-consumer-group";
     public int SessionTimeoutMs { get; set; } = 10_000;
     public int MaxPollIntervalMs { get; set; } = 300_000;
